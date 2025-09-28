7e927c301807c8e864ba810ebbb2c1c1b355c4a5
Hussain <althafhusssaian@gmail.com>
2025-09-28 12:51:27 +0530

    install redux

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import CheckoutPage from './pages/CheckoutPage';
+import MyCartSummary from './components/MyCartSummary';
+import ProductCard from './components/ProductCard';
 
 
 function App() {
   return (
     <div >
-
+<Header/>
       <Routes>
-        <Route path='/Home' element = {<Header />} />
+       
+         <Route path="/" element={<HomePage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
        
      </Routes>
+     <MyCartSummary/>
+    
       <Footer />
 
     
---
+++ src/data/products.js
@@ -0,0 +1,10 @@
+
+const PRODUCTS = [
+  { id: 1, name: "Dr. Scholl's Time Off Sneaker", price: 50, image: "img2.jpg" },
+  { id: 2, name: "Adidas Run Falcon 5 Sneaker", price: 80, image: "img3.jpg" },
+  { id: 3, name: "Madden Girl Women's Giia Sneaker", price: 30, image: "img4.jpg" },
+  { id: 4, name: "New Balance Men's DynaSoft Vitrel", price: 60, image: "img5.jpg" },
+  
+];
+
+export default PRODUCTS;
