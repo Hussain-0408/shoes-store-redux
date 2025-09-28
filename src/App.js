15162279822b261aa778d78baababc9a071ac6b3
Hussain <althafhusssaian@gmail.com>
2025-09-28 11:23:44 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+// import { Suspense,lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div >
+
+      <Routes>
+        <Route path='/Home' element = {<Header />} />
+     </Routes>
+    
       
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,92 @@
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+      <div className="container-fluid">
+       
+        <NavLink className="navbar-brand fw-bold  text-uppercase" to="/">
+          Shoe Store
+        </NavLink>
+
+      
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+
+            <li className="nav-item">
+              <NavLink className="nav-link fw-bold text-white" to="/">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item dropdown">
+              <NavLink
+                className="nav-link dropdown-toggle"
+               
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Categories
+              </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <NavLink className="dropdown-item" to="/mens">
+                    Mens
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/womens">
+                    Womens
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/kids">
+                    Kids Section
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/latest">
+                    Latest
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link fw-bold text-white" to="/about">
+                About Us
+              </NavLink>
+            </li>
+          </ul>
+          <form className="d-flex ms-lg-3 mt-2 mt-lg-0 ">
+            <input
+              className="form-control me-2 "
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success fw-bold text-white" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
