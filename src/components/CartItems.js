d4e1d647e02fc57e85a9ea4bf035966c5bae17a2
Hussain <althafhusssaian@gmail.com>
2025-09-30 23:58:49 +0530

    added back to shop logic

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row ,Col} from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
 function CartItems() {
-    const navigate = useNavigate("/PaymentPage")
+    const[shop , setShop] = useState(false)
+    const navigate = useNavigate();
     const handlenavigate = ()=>{
         navigate("/PaymentPage")
+        setShop(false)
+
     }
+    const handleback =()=>{
+        navigate("/");
+        setShop(true)
+    };
+    
   return (
     <div>
         <Container>
             <Row> 
                 <Col xs={12} md={6} lg={12}>
 
-                 <div className='bg-white border w-100 justify-content-center d-flex py-2 mb-0 '>
-                    <div className='py-2 d-flex  gap-3 '>
+                 <div className='bg-white border w-100 justify-content-center d-flex  mb-0 '>
+                    <div className='p-3 d-flex  gap-3 '>
                         <h2>Total:$</h2>
-                        <button className='btn btn-success btn-outline-danger text-white' onClick={handlenavigate}> Proced to Payment</button>
+                     {shop ? (   <button className='btn btn-success btn-outline-danger text-white' onClick={handlenavigate}> Proced to Payment</button>) :
+                                 (<button className='btn btn-success btn-outline-danger text-white' onClick={handleback}> Back to Shopping</button>)}
                     </div>
 
                  </div>
